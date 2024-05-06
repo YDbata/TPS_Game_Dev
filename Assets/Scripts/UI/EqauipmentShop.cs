d890d9767a9d3417a7566a2f0c361ee5497458a7
Y_ik sun <51112432+YDbata@users.noreply.github.com>
2024-05-06 17:27:45 +0900

    feat : merge를 위한 push
    장비 고치는 중

--- Assets/Scripts/UI/EqauipmentShop.cs
+++ Assets/Scripts/UI/EqauipmentShop.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TPSGame.UI;
 using UnityEngine;
 
-public class EqauipmentShop : MonoBehaviour
+public class EqauipmentShop : UIBase
 {
     // Start is called before the first frame update
     void Start()
